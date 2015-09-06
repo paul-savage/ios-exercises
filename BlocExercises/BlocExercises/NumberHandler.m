86381fcc4637cda22dec8fb81712f9779d2c4087
Paul Savage <p.a.savage@btinternet.com>
2015-09-06 16:04:12 +0100

    Assignment 15 completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,38 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    
+    return [NSNumber numberWithInteger:[number integerValue] * 2];
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    
+    NSInteger lowerBound = (number <= otherNumber) ? number : otherNumber;
+    NSInteger upperBound = (otherNumber >= number) ? otherNumber : number;
+    
+    NSMutableArray *numbers = [NSMutableArray array];
+    
+    for (NSInteger n = lowerBound; n <= upperBound; n++) {
+        [numbers addObject:[NSNumber numberWithInteger:n]];
+    }
+    
+    return [numbers copy];
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    
+    NSInteger lowestNumber = [arrayOfNumbers[0] integerValue];
+    
+    for (NSNumber *nextNumber in arrayOfNumbers) {
+        
+        NSInteger nextValue = [nextNumber integerValue];
+        
+        if (nextValue < lowestNumber) {
+            lowestNumber = nextValue;
+        }
+    }
+    
+    return lowestNumber;
 }
 
 @end
